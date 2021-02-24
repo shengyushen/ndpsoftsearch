b5b28f52f1fd56f7975660b9e01f6d41fc651c4f
shengyushen <shengyushen@aliyun.com>
2021-02-24 01:32:44 -0500

    adding fix1

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-latex main.tex; bibtex main ; latex main.tex;latex main.tex; dvipdfm main.dvi
-
+# latex main.tex; bibtex main ; latex main.tex;latex main.tex; dvipdfm main.dvi
+pdflatex main.tex; bibtex main ; pdflatex main.tex; pdflatex main.tex
